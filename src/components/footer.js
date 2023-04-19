5ae51a2f67a983b8fecaa64b966c51eba2bb5dca
Reza <mrneyestani@gmail.com>
2023-04-19 19:56:04 +0200

    modify designed

--- src/components/footer.js
+++ src/components/footer.js
@@ -111,7 +111,7 @@ const Footer = () => {
         <a href="https://github.com/bchiang7/v4">
           <div>Designed by Brittany Chiang</div>
 
-          {githubInfo.stars && githubInfo.forks && (
+          {/* {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
               <span>
                 <Icon name="Star" />
@@ -122,7 +122,7 @@ const Footer = () => {
                 <span>{githubInfo.forks.toLocaleString()}</span>
               </span>
             </div>
-          )}
+          )} */}
         </a>
       </StyledCredit>
     </StyledFooter>
