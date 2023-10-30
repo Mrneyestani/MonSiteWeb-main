f8529c1d0febec24678aecc54d5b25a5a2386241
Reza <mrneyestani@gmail.com>
2023-10-30 23:06:59 +0100

    Modify explanation

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -64,7 +64,7 @@ const Hero = () => {
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
-      <p>I am a full-stack developer and I'm currently looking for a job.</p>
+      <p>I am a passionate and motivated developer and I'm currently looking for a job.</p>
     </>
   );
   // const five = (
